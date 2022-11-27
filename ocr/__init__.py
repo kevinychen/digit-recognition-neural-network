c9ac8248ce615906cfce312fc4c998df116a43f7
Kevin Y. Chen <kyc2915@gmail.com>
2022-11-26 21:15:53 -0800

    batches

--- ocr/__init__.py
+++ ocr/__init__.py
@@ -22,35 +22,34 @@ images, labels = extract_training_samples('digits')
 
 shape = [784] + [20] * 2 + [10]
 num_levels = len(shape) - 1
-mult = .01
+mult = .001
 train = 100000
+batch = 100
 
 weights = [np.random.normal(size=(shape[lvl + 1], shape[lvl])) for lvl in range(num_levels)]
 biases = [np.zeros(shape[lvl + 1]) for lvl in range(num_levels)]
-for n in range(train):
-    neurons = [np.vectorize(lambda x: x / 256)(images[n].flatten())]
-    for lvl in range(num_levels):
-        neurons.append(np.vectorize(sigma)(np.matmul(weights[lvl], neurons[lvl]) + biases[lvl]))
+for loop in range(5):
+    for n in range(0, train, batch):
+        neurons = [np.array([np.vectorize(lambda x: x / 256)(image.flatten()) for image in images[n:n+batch]])]
+        for lvl in range(num_levels):
+            neurons.append(np.vectorize(sigma)(neurons[lvl] @ weights[lvl].T) + biases[lvl])
 
-    expected = [1 if i == labels[n] - 1 else 0 for i in range(shape[-1])]
-    dE_da = 2 * (neurons[-1] - expected)
-    for lvl in range(num_levels, 0, -1):
-        for i in range(len(neurons[lvl])):
-            da_dw = dsigma(neurons[lvl][i]) * neurons[lvl - 1]
-            weights[lvl - 1][i] -= mult * dE_da[i] * da_dw
-        da_db = np.vectorize(dsigma)(neurons[lvl])
-        biases[lvl - 1] -= mult * dE_da * da_db
-        dE_da = np.matmul(np.transpose(weights[lvl - 1]), dE_da * np.vectorize(dsigma)(neurons[lvl]))
+        expected = [[1 if i == label - 1 else 0 for i in range(shape[-1])] for label in labels[n:n+batch]]
+        dE_da = neurons[-1] - expected
+        for lvl in range(num_levels)[::-1]:
+            dE_dv = dE_da * np.vectorize(dsigma)(neurons[lvl + 1])
+            dE_da = dE_dv @ weights[lvl]
+            weights[lvl] -= mult * dE_dv.T @ neurons[lvl]
+            biases[lvl] -= mult * np.sum(dE_dv, axis=0)
 
-    error = sum((neurons[-1] - expected) ** 2)
-    if n % 1000 == 0:
-        print(n, error)
+        error = np.sum((neurons[-1] - expected) ** 2)
+        if n % 10000 == 0:
+            print(n, error)
 
 test = []
 for n in range(train, train + 10000):
     neurons = [np.vectorize(lambda x: x / 256)(images[n].flatten())]
     for lvl in range(num_levels):
-        neurons.append(np.vectorize(sigma)(np.matmul(weights[lvl], neurons[lvl]) + biases[lvl]))
-
+        neurons.append(np.vectorize(sigma)(weights[lvl] @ neurons[lvl]) + biases[lvl])
     test.append(np.argmax(neurons[-1]) == labels[n] - 1)
 print(sum(test), len(test))
